c6b5dc05106ba0075e7a1e3dc978525f6f0eb6f5
aufdj <68519879+aufdj@users.noreply.github.com>
2021-12-26 19:30:59 -0500

    Update huffman.rs

--- huffman.rs
+++ huffman.rs
@@ -1,28 +1,139 @@
-use std::collections::BinaryHeap;
-use std::collections::BTreeMap;
-use std::fs::File;
-use std::io::{Read, Write, BufReader, BufWriter, BufRead, Seek};
-use std::env;
-use std::fs::metadata;
-use std::time::Instant;
-use std::path::Path;
+use std::{
+    collections::{BinaryHeap, BTreeMap},
+    fs::{File, metadata},
+    io::{Read, Write, BufReader, BufWriter, BufRead, Seek},
+    env,
+    time::Instant,
+    path::Path,
+    cmp::Ordering,
+};
 
-// Convenience functions for buffered IO ---------------------------
-fn write(file_out: &mut BufWriter<File>, output: u8) {
-    file_out.write(&[output]).unwrap();
-    if file_out.buffer().len() >= file_out.capacity() { 
-        file_out.flush().unwrap(); 
+// Convenience functions for buffered I/O ---------------------------------------------------------- Convenience functions for buffered I/O
+#[derive(PartialEq, Eq)]
+enum BufferState {
+    NotEmpty,
+    Empty,
+}
+
+trait BufferedRead {
+    fn read_byte(&mut self, input: &mut [u8; 1]);
+    fn read_u32(&mut self, input: &mut [u8; 4]);
+    fn fill_buffer(&mut self) -> BufferState;
+}
+impl BufferedRead for BufReader<File> {
+    fn read_byte(&mut self, input: &mut [u8; 1]) {
+        match self.read(input) {
+            Ok(_)  => {},
+            Err(e) => { 
+                println!("Function read_byte failed."); 
+                println!("Error: {}", e);
+            },
+        };
+        if self.buffer().is_empty() { 
+            self.consume(self.capacity()); 
+            match self.fill_buf() {
+                Ok(_)  => {},
+                Err(e) => {
+                    println!("Function read_byte failed.");
+                    println!("Error: {}", e);
+                },
+            }
+        }
+    }
+    fn read_u32(&mut self, input: &mut [u8; 4]) {
+        match self.read(input) {
+            Ok(_)  => {},
+            Err(e) => { 
+                println!("Function read_u32 failed."); 
+                println!("Error: {}", e);
+            },
+        };
+        if self.buffer().is_empty() { 
+            self.consume(self.capacity()); 
+            match self.fill_buf() {
+                Ok(_)  => {},
+                Err(e) => { 
+                    println!("Function read_u32 failed."); 
+                    println!("Error: {}", e);
+                },
+            }
+        }
+    }
+    fn fill_buffer(&mut self) -> BufferState {
+        self.consume(self.capacity());
+        match self.fill_buf() {
+            Ok(_)  => {},
+            Err(e) => { 
+                println!("Function fill_buffer failed."); 
+                println!("Error: {}", e);
+            },
+        }
+        if self.buffer().is_empty() { 
+            return BufferState::Empty; 
+        }
+        BufferState::NotEmpty
     }
 }
-fn read(buf_in: &mut BufReader<File>, input: &mut [u8; 4]) -> usize {
-    let bytes_read = buf_in.read(input).unwrap();
-    if buf_in.buffer().len() <= 0 { 
-        buf_in.consume(buf_in.capacity()); 
-        buf_in.fill_buf().unwrap();
-    }
-    bytes_read
+trait BufferedWrite {
+    fn write_byte(&mut self, output: u8);
+    fn write_u32(&mut self, output: u32);
+    fn flush_buffer(&mut self);
 }
-// -----------------------------------------------------------------
+impl BufferedWrite for BufWriter<File> {
+    fn write_byte(&mut self, output: u8) {
+        match self.write(&[output]) {
+            Ok(_)  => {},
+            Err(e) => { 
+                println!("Function write_byte failed."); 
+                println!("Error: {}", e);
+            },
+        }
+        if self.buffer().len() >= self.capacity() { 
+            match self.flush() {
+                Ok(_)  => {},
+                Err(e) => { 
+                    println!("Function write_byte failed."); 
+                    println!("Error: {}", e);
+                },
+            } 
+        }
+    }
+    fn write_u32(&mut self, output: u32) {
+        match self.write(&output.to_le_bytes()[..]) {
+            Ok(_)  => {},
+            Err(e) => { 
+                println!("Function write_usize failed."); 
+                println!("Error: {}", e);
+            },
+        }
+        if self.buffer().len() >= self.capacity() { 
+            match self.flush() {
+                Ok(_)  => {},
+                Err(e) => { 
+                    println!("Function write_usize failed."); 
+                    println!("Error: {}", e);
+                },
+            } 
+        }
+    }
+    fn flush_buffer(&mut self) {
+        match self.flush() {
+            Ok(_)  => {},
+            Err(e) => { 
+                println!("Function flush_buffer failed."); 
+                println!("Error: {}", e);
+            },
+        }    
+    }
+}
+fn new_input_file(capacity: usize, file_name: &str) -> BufReader<File> {
+    BufReader::with_capacity(capacity, File::open(file_name).unwrap())
+}
+fn new_output_file(capacity: usize, file_name: &str) -> BufWriter<File> {
+    BufWriter::with_capacity(capacity, File::create(file_name).unwrap())
+}
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
 
 // Node implementation ---------------------------------------------
 #[derive(Eq, PartialEq)]
@@ -41,12 +152,12 @@ impl Node {
     }
 }
 impl Ord for Node {
-    fn cmp(&self, rhs: &Self) -> std::cmp::Ordering {
+    fn cmp(&self, rhs: &Self) -> Ordering {
         rhs.frequency.cmp(&self.frequency)
     }
 } 
 impl PartialOrd for Node {
-    fn partial_cmp(&self, rhs: &Self) -> Option<std::cmp::Ordering> {
+    fn partial_cmp(&self, rhs: &Self) -> Option<Ordering> {
         Some(self.cmp(&rhs))
     }
 }
@@ -54,17 +165,13 @@ impl PartialOrd for Node {
 
 // Model data to get frequency distribution ------------------------
 fn model(file_in: &mut BufReader<File>) -> [u32; 256] {
-    file_in.fill_buf().unwrap();
+    file_in.fill_buffer();
     let mut frequencies = [1u32; 256];
     loop {
-        for i in 0..file_in.buffer().len() {
-            frequencies[file_in.buffer()[i as usize] as usize] += 1;
+        for byte in file_in.buffer().iter() {
+            frequencies[*byte as usize] += 1;
         }
-        file_in.consume(file_in.capacity());
-        file_in.fill_buf().unwrap();
-        if file_in.buffer().is_empty() {
-            break;
-        }
+        if file_in.fill_buffer() == BufferState::Empty { break; }
     }
     frequencies
 }
@@ -89,32 +196,32 @@ fn build_tree(heap: &mut BinaryHeap<Node>) {
 type HuffmanCodeMap  = BTreeMap<u8, Vec<u8>>;
 type HuffmanCodeMapD = BTreeMap<Vec<u8>, u8>;
 
-fn generate_codes(node: &Node, prefix: Vec<u8>, codes: &mut HuffmanCodeMap) {
+fn gen_codes(node: &Node, prefix: Vec<u8>, codes: &mut HuffmanCodeMap) {
     match node.node_type {
         NodeType::Internal(ref left_child, ref right_child) => {
             let mut left_prefix = prefix.clone();
             left_prefix.push(0);
-            generate_codes(&left_child, left_prefix, codes);
+            gen_codes(&left_child, left_prefix, codes);
 
             let mut right_prefix = prefix;
             right_prefix.push(1);
-            generate_codes(&right_child, right_prefix, codes);
+            gen_codes(&right_child, right_prefix, codes);
         }
         NodeType::Leaf(byte) => {
             codes.insert(byte, prefix);
         }
     }
 }
-fn generate_codes_d(node: &Node, prefix: Vec<u8>, codes: &mut HuffmanCodeMapD) {
+fn gen_codes_d(node: &Node, prefix: Vec<u8>, codes: &mut HuffmanCodeMapD) {
     match node.node_type {
         NodeType::Internal(ref left_child, ref right_child) => {
             let mut left_prefix = prefix.clone();
             left_prefix.push(0);
-            generate_codes_d(&left_child, left_prefix, codes);
+            gen_codes_d(&left_child, left_prefix, codes);
 
             let mut right_prefix = prefix;
             right_prefix.push(1);
-            generate_codes_d(&right_child, right_prefix, codes);
+            gen_codes_d(&right_child, right_prefix, codes);
         }
         NodeType::Leaf(byte) => {
             codes.insert(prefix, byte);
@@ -124,19 +231,16 @@ fn generate_codes_d(node: &Node, prefix: Vec<u8>, codes: &mut HuffmanCodeMapD) {
 // -----------------------------------------------------------------
 
 fn compress(file_in: &mut BufReader<File>, file_out: &mut BufWriter<File>) {
-    write(file_out, 0);
+    file_out.write_byte(0);
 
-    let frequencies: [u32; 256] = model(file_in);                   // model data to get prob. distribution
-
-    for i in 0..256 {                                               // include model as compressed data header
-        for j in 0..4 {                                             //
-            write(file_out,                                         //
-                ((frequencies[i] >> (j*8)) & 0xFF) as u8);          //
-        }                                                           //
+    let frequencies: [u32; 256] = model(file_in);                   // Model data to get prob. distribution
+     
+    for freq in frequencies.iter() {                                // Include model as compressed data header
+        file_out.write_u32(*freq);                                  //                                                               
     }                                                               //
     
     let mut heap: BinaryHeap<Node> = BinaryHeap::new();
-    for i in 0..256 {                                               // add leaf nodes to heap
+    for i in 0..256 {                                               // Add leaf nodes to heap
         heap.push(                                                  //
             Node::new(frequencies[i], NodeType::Leaf(i as u8))      //
         );                                                          //
@@ -144,20 +248,20 @@ fn compress(file_in: &mut BufReader<File>, file_out: &mut BufWriter<File>) {
 
     build_tree(&mut heap);       
 
-    let mut codes = HuffmanCodeMap::new();                          // walk down tree and generate codes
-    generate_codes(heap.peek().unwrap(), vec![0u8; 0], &mut codes); // 
+    let mut codes = HuffmanCodeMap::new();                          // Walk down tree and generate codes
+    gen_codes(heap.peek().unwrap(), vec![0u8; 0], &mut codes);      // 
 
-    file_in.get_ref().rewind().unwrap();
-    file_in.fill_buf().unwrap();
+    file_in.rewind().unwrap();
+    file_in.fill_buffer();
 
     let mut packed_codes: u8 = 0;
     let mut num_bits: u8 = 0;
     loop {
-        for i in 0..file_in.buffer().len() { 
+        for byte in file_in.buffer().iter() { 
             // Get huffman code corresponding to current byte and write bits to output
-            for bit in codes.get(&file_in.buffer()[i]).unwrap() {
+            for bit in codes.get(byte).unwrap() {
                 if num_bits >= 8 {
-                    write(file_out, packed_codes);
+                    file_out.write_byte(packed_codes);
                     packed_codes = 0;
                     num_bits = 0;
                 }
@@ -166,18 +270,14 @@ fn compress(file_in: &mut BufReader<File>, file_out: &mut BufWriter<File>) {
             }
         }
         
-        file_in.consume(file_in.capacity());
-        file_in.fill_buf().unwrap();
-        if file_in.buffer().is_empty() {
+        if file_in.fill_buffer() == BufferState::Empty {
             if num_bits > 0 {
-                write(file_out, packed_codes);
+                file_out.write_byte(packed_codes);
             }
-            file_out.flush().unwrap();
-
-            file_out.get_ref().rewind().unwrap();
-            write(file_out, 8 - num_bits);
-            file_out.flush().unwrap();
-
+            file_out.flush_buffer();
+            file_out.rewind().unwrap();
+            file_out.write_byte(8 - num_bits);
+            file_out.flush_buffer();
             println!("Finished Compressing.");
             break;
         }
@@ -186,18 +286,17 @@ fn compress(file_in: &mut BufReader<File>, file_out: &mut BufWriter<File>) {
 
 fn decompress(file_in: &mut BufReader<File>, file_out: &mut BufWriter<File>, file_in_size: u64) {
     let mut padding = [0u8; 1];
-    file_in.read(&mut padding).unwrap();
+    file_in.read_byte(&mut padding);
 
     let mut frequencies = [0u32; 256];
     let mut frequency = [0; 4];
 
     for i in 0..256 {
-        read(file_in, &mut frequency);
-        frequencies[i] = (frequency[0] as u32)        + ((frequency[1] as u32) << 8)  
-                      + ((frequency[2] as u32) << 16) + ((frequency[3] as u32) << 24);    
+        file_in.read_u32(&mut frequency);
+        frequencies[i] = u32::from_le_bytes(frequency);    
     }
 
-    let mut heap: BinaryHeap<Node> = BinaryHeap::new();
+    let mut heap: BinaryHeap<Node> = BinaryHeap::with_capacity(512);
     for i in 0..256 {                                               
         heap.push(                                                  
             Node::new(frequencies[i], NodeType::Leaf(i as u8))     
@@ -207,38 +306,36 @@ fn decompress(file_in: &mut BufReader<File>, file_out: &mut BufWriter<File>, fil
     build_tree(&mut heap); 
 
     let mut codes = HuffmanCodeMapD::new();
-    generate_codes_d(heap.peek().unwrap(), vec![0u8; 0], &mut codes);
+    gen_codes_d(heap.peek().unwrap(), vec![0u8; 0], &mut codes);
 
-    file_in.fill_buf().unwrap();
-    let mut current_code: Vec<u8> = Vec::new();
-
+    let mut curr_code: Vec<u8> = Vec::with_capacity(8);
     let mut pos = 1026;
-
+    file_in.fill_buf().unwrap(); // Don't use fill_buffer() here 
+    
     loop {
-        for i in 0..file_in.buffer().len() {
+        for byte in file_in.buffer().iter() {
             if pos >= file_in_size { 
                 for j in (0..=(7 - padding[0])).rev() {
-                    current_code.push((file_in.buffer()[i] >> j) & 1);
-                    if codes.contains_key(&current_code) {
-                        write(file_out, *codes.get(&current_code).unwrap());
-                        current_code.clear();
+                    curr_code.push((*byte >> j) & 1);
+                    if codes.contains_key(&curr_code) {
+                        file_out.write_byte(*codes.get(&curr_code).unwrap());
+                        curr_code.clear();
                     }
                 }
-            } else {
+            } 
+            else {
                 for j in (0..=7).rev() {
-                    current_code.push((file_in.buffer()[i] >> j) & 1);
-                    if codes.contains_key(&current_code) {
-                        write(file_out, *codes.get(&current_code).unwrap());
-                        current_code.clear();
+                    curr_code.push((*byte >> j) & 1);
+                    if codes.contains_key(&curr_code) {
+                        file_out.write_byte(*codes.get(&curr_code).unwrap());
+                        curr_code.clear();
                     }
                 }
             }
             pos += 1;
         }
-        file_in.consume(file_in.capacity());
-        file_in.fill_buf().unwrap();
-        if file_in.buffer().is_empty() {
-            file_out.flush().unwrap();
+        if file_in.fill_buffer() == BufferState::Empty {
+            file_out.flush_buffer();
             println!("Finished Decompressing.");
             break;
         }
@@ -248,21 +345,23 @@ fn decompress(file_in: &mut BufReader<File>, file_out: &mut BufWriter<File>, fil
 fn main() {
     let start_time = Instant::now();
     let args: Vec<String> = env::args().collect();
-    let mut file_in  = BufReader::with_capacity(4096, File::open(&args[2]).unwrap());
-    let mut file_out = BufWriter::with_capacity(4096, File::create(&args[3]).unwrap());
+    let mut file_in  = new_input_file(4096, &args[2]);
+    let mut file_out = new_output_file(4096, &args[3]);
 
     match (&args[1]).as_str() {
         "c" => { 
             let file_in_size = metadata(Path::new(&args[2])).unwrap().len();
             compress(&mut file_in, &mut file_out);
             let file_out_size = metadata(Path::new(&args[3])).unwrap().len();  
-            println!("{} bytes -> {} bytes in {:.2?}", file_in_size, file_out_size, start_time.elapsed());   
+            println!("{} bytes -> {} bytes in {:.2?}", 
+            file_in_size, file_out_size, start_time.elapsed());   
         },
         "d" => { 
             let file_in_size = metadata(Path::new(&args[2])).unwrap().len();
             decompress(&mut file_in, &mut file_out, file_in_size);
             let file_out_size = metadata(Path::new(&args[3])).unwrap().len();  
-            println!("{} bytes -> {} bytes in {:.2?}", file_in_size, file_out_size, start_time.elapsed());   
+            println!("{} bytes -> {} bytes in {:.2?}", 
+            file_in_size, file_out_size, start_time.elapsed());   
         }, 
         _ => {
             println!("To compress: c input output");
@@ -270,3 +369,4 @@ fn main() {
         }
     }    
 }
+
